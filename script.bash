5db96c502f0370fc2cb6865a2b779c729be7bc51
Niamh Driscoll <niamhmolson@outlook.com>
2025-12-04 11:10:48 -0800

    Delete script.bash

--- script.bash
+++
@@ -1,6 +0,0 @@
-echo "This will run MyFlock 0.9. Please make sure you have Python 3 installed. Please make sure the file loaction is /home/nolson/Coding/MyFlock 0.9/MyFlock-0.9. If it is not, move it or change this file. The app will start in 10 seconds."  
-sleep 10
-cd "/home/nolson/Coding/MyFlock 0.9/MyFlock-0.9"
-echo "Starting MyFlock 0.9..."
-python3 app.py
-echo "MyFlock 0.9 has shut down due to an error or a user request."
